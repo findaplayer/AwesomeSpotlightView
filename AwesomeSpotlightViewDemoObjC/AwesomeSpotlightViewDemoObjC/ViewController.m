6e22395248faeef8543dc1f1bbcd6145687778a5
Duncan Brown <brown.duncan.73@gmail.com>
2021-08-24 16:44:45 +0100

    update

--- AwesomeSpotlightViewDemoObjC/AwesomeSpotlightViewDemoObjC/ViewController.m
+++ AwesomeSpotlightViewDemoObjC/AwesomeSpotlightViewDemoObjC/ViewController.m
@@ -53,17 +53,18 @@
     
     CGRect logoImageViewSpotlightRect = CGRectMake(self.logoImageView.frame.origin.x, self.logoImageView.frame.origin.y, self.logoImageView.frame.size.width, self.logoImageView.frame.size.height);
     UIEdgeInsets logoImageViewSpotlightMargin = UIEdgeInsetsMake(20.0, 20.0, 20.0, 20.0);
-    AwesomeSpotlight *logoImageViewSpotlight = [[AwesomeSpotlight alloc] initWithRect:logoImageViewSpotlightRect shape:AwesomeSpotlightShapeCircle text:@"logoImageViewSpotlight" margin:logoImageViewSpotlightMargin isAllowPassTouchesThroughSpotlight:false];
+    AwesomeSpotlight *logoImageViewSpotlight = [[AwesomeSpotlight alloc] initWithRect:logoImageViewSpotlightRect shape:AwesomeSpotlightShapeCircle text:@"logo Image View Spotlight" margin:logoImageViewSpotlightMargin isAllowPassTouchesThroughSpotlight:false];
     
-    AwesomeSpotlight *nameLabelSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.nameLabel.frame shape:AwesomeSpotlightShapeRectangle text:@"nameLabelSpotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
+    AwesomeSpotlight *nameLabelSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.nameLabel.frame shape:AwesomeSpotlightShapeRectangle text:@"name Label Spotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
     
-    AwesomeSpotlight *showButtonSpotSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.showButton.frame shape:AwesomeSpotlightShapeRoundRectangle text:@"showButtonSpotSpotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
+    AwesomeSpotlight *showButtonSpotSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.showButton.frame shape:AwesomeSpotlightShapeRoundRectangle text:@"show Button Spot Spotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
 
-    AwesomeSpotlight *showWithContinueAndSkipButtonSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.showWithContinueAndSkipButton.frame shape:AwesomeSpotlightShapeRoundRectangle text:@"showWithContinueAndSkipButtonSpotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
+    AwesomeSpotlight *showWithContinueAndSkipButtonSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.showWithContinueAndSkipButton.frame shape:AwesomeSpotlightShapeRoundRectangle text:@"show With Continue\nAnd Skip Button Spotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
     
-    AwesomeSpotlight *showAllAtOnceButtonSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.showAllAtOnceButton.frame shape:AwesomeSpotlightShapeRoundRectangle text:@"showAllAtOnceButtonSpotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
+    AwesomeSpotlight *showAllAtOnceButtonSpotlight = [[AwesomeSpotlight alloc] initWithRect:self.showAllAtOnceButton.frame shape:AwesomeSpotlightShapeRoundRectangle text:@"show All At Once Button Spotlight" margin:spotlightMarginDefault isAllowPassTouchesThroughSpotlight:false];
     
     self.spotlightView = [[AwesomeSpotlightView alloc] initWithFrame:self.view.frame spotlight:@[logoImageViewSpotlight, nameLabelSpotlight, showButtonSpotSpotlight, showWithContinueAndSkipButtonSpotlight, showAllAtOnceButtonSpotlight]];
+	self.spotlightView.alignment = NSTextAlignmentLeft;
     self.spotlightView.cutoutRadius = 8;
     self.spotlightView.delegate = self;
 }
